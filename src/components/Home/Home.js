2c5d511c3c8b0ad425bdf9b19a6d5b4ac3fcf8ed
taibislamdipu <mailtaibislam@gmail.com>
2020-10-03 06:26:52 +0600

    home header done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,8 +7,15 @@ const Home = () => {
 
         <main>
             <Header></Header>
-            <div className="container mt-5">
-                <h1>This is the home page</h1>
+            <div className="container mt-5 text-center">
+                <h1>I grow by helping people in need.</h1>
+
+                {/* <nav class="navbar navbar-light bg-light"> */}
+                <form class="form-inline d-flex justify-content-center mt-4">
+                    <input class="form-control mr-sm-2 w-25" type="search" placeholder="Search..." aria-label="Search" />
+                    <button class="btn btn-primary my-2 my-sm-0" type="submit">Search</button>
+                </form>
+                {/* </nav> */}
             </div>
 
         </main>
